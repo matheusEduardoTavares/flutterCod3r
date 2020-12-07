c167dae79cbebdebf6c93b6363cf4326b7f0a9d6
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-12-07 20:45:09 -0300

    Aula 438 - Desafio If/Else - finalizada

---
+++ curso_dart/controle/desafio_if_else.dart
@@ -0,0 +1,14 @@
+void main(List<String> args){
+  var nota = 2.3;
+
+  //NÃO USA ; EM ESTRUTURAS DE CONTROLE *
+  if (nota >= 9.0); {
+    print('Parabéns! Vc foi brilhante!');
+  }
+
+  if (nota >= 9.0) {
+    print('Parabéns! Vc foi brilhante!');
+  }
+
+  // * EXCEÇÃO DO/WHILE!!!
+}
