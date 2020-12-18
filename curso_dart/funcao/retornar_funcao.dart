b9e39a7645be9b9b4da3b7ddd02cbece3a398293
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-12-18 02:17:55 -0300

    Aula 459 - Retornar Função #02 - finalizada

---
+++ curso_dart/funcao/retornar_funcao.dart
@@ -0,0 +1,19 @@
+int Function(int) somaParcial(int a){
+  int c = 0;
+
+  return (int b) {
+    return a + b + c;
+  };
+}
+
+void main(List<String> args){
+  print(somaParcial(2));
+  // Closure: (int) => int
+  print(somaParcial(2)(10));
+
+  var somaCom10 = somaParcial(10);
+  
+  print(somaCom10(3));
+  print(somaCom10(7));
+  print(somaCom10(19));
+}
