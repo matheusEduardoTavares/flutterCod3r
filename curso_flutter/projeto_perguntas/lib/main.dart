d5f314b85fb62d81913904c14ff361b700c0087d
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-12-20 18:12:32 -0300

    Aula 25 - Resumo até Aqui - finalizada

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(PerguntaApp());
-}
+//Esse primeiro componente que passamos para o runApp
+//é o nó raíz da árvore de componentes da aplicação,
+//todo resto estará embaixo desse componente raíz, 
+//e o componente tem esse método build que recebe
+//por parâmetro o BuildContext, e quem passa esse 
+//parâmetro é o próprio flutter. Cada componente 
+//tem seu próprio contexto, e o componente filho 
+//tem como a partir do contexto, o contexto tem a 
+//referência para o contexto do pai, e dessa forma
+//é possível que um componente pai se comunique com
+//um componente filho a partir deste contexto. Mas
+//cada componente tem seu próprio contexto.
+main() => runApp(PerguntaApp());
+
+//O componente raíz dessa aplicação, portanto, é o 
+//PerguntaApp, e ele tem como filho o MaterialApp.
 
 class PerguntaApp extends StatelessWidget{
+  //O @override é um decorator para sobrescrever
+  //um método que obrigatoriamente o componente
+  //stateless precisa implementar, que é o 
+  //método build. É o flutter quem chama esse 
+  //método.
+
   @override 
   Widget build(BuildContext context){
+    //A criação do nosso widget é o MaterialApp
     return MaterialApp(
       title: 'PerguntaApp',
       home: Text('Olá Flutter!!!')
