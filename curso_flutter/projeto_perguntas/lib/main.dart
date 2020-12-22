c134f29d0a050f94c4eb385f63ab38ecbd4791b7
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-12-22 18:41:36 -0300

    Aula 32 - Variáveis em StatelessWidget - finalizada

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -24,8 +24,28 @@ class PerguntaApp extends StatelessWidget{
   //método build. É o flutter quem chama esse 
   //método.
 
+  //Classes que extendem o StatelessWidget deve m
+  //ser imutáveis, ou seja, ter atributos apenas final,
+  //isso já gera um warning, pois se é preciso haver uma
+  //variável cujo valor será alterado, é preciso ter 
+  //um estado para alterá-la, então a classe deve 
+  //extender o StatefulWidget.
+  var perguntaSelecionada = 0;
+
   void responder() {
-    print('Pergunta respondida');
+    /*
+      No flutter temos uma ideia de interface reativa,
+      no momento que alteramos o valor de uma variável
+      de forma reativa será gerado uma atualização na UI
+      de forma que conseguimos ver o último estado sendo
+      renderizado na tela. Não está acontecendo nada pois
+      estamos dentro de um componente stateless, dito
+      sem estado. O fato de termos uma variável sendo 
+      alterada dentro de um componente stateless é errado
+      , violamos essa ideia desse componente.
+    */
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
   }
 
   @override 
@@ -47,7 +67,7 @@ class PerguntaApp extends StatelessWidget{
           //Não precisa pow <Widget> (generics) para
           //explicitar o tipo da lista.
           children: [
-            Text(perguntas[0]),
+            Text(perguntas[perguntaSelecionada]),
             RaisedButton(
               child: Text('Resposta 1'),
               //Se deixássemos assim:
@@ -74,15 +94,13 @@ class PerguntaApp extends StatelessWidget{
               //parâmetros, justamente o que precisamos
               //passar para o onPressed
               // onPressed: funcaoQueRetornaUmaOutraFuncao()
-              onPressed: () {
-                print('Resposta 2 foi selecionada!');
-              }
+              onPressed: responder
               //O botão só fica habilitado para clique
               //caso seu onPressed seja != null
             ),
             RaisedButton(
               child: Text('Resposta 3'),
-              onPressed: () => print('Resposta 3 foi selecionada!')
+              onPressed: responder
             ),
             //Portanto ou passamos como referência uma
             //função que criamos ou passamos a função
