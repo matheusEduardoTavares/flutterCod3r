b35161f6536bfefffd4e2d34d885923d25adb875
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-12-22 21:13:11 -0300

    Aula 39 - Aplicando Estilo ao Widget Questão - finalizada

--- curso_flutter/projeto_perguntas/lib/questao.dart
+++ curso_flutter/projeto_perguntas/lib/questao.dart
@@ -7,6 +7,30 @@ class Questao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        texto,
+        style: TextStyle(fontSize: 28),
+        //Só colocar isso não muda nada pois o Text terá
+        //exatamente o mesmo tamanho daquilo que é 
+        //visível na tela, ou seja, o tamanho do texto
+        //aumenta de acordo com seu conteúdo, e quando
+        //pedimos para centralizar algo dentro de algo
+        //que já está sendo ocupado 100%, por isso não
+        //centraliza na tela. Para poder centralizar 
+        //então, faremos um wrap no Text com um 
+        //container, mas o resultado será o mesmo pois
+        //o container tem o mesmo tamanho do seu
+        //conteúdo caso não seja passado um width ou
+        //um height. Para fazer pegar a largura inteira
+        //da tela, basta usarmos para o width o valor
+        //double.infinity, estando com esse width definido
+        //sem o TextAlign o texto volta para o começo e não
+        //para o centro da tela.
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
