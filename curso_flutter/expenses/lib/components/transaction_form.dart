bc2bb5f620c3a15aa0da81270860f62e6492f2d8
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-01-02 23:35:49 -0300

    Aula 158 - Ciclo de Vida dos Widgets - finalizada

--- curso_flutter/expenses/lib/components/transaction_form.dart
+++ curso_flutter/expenses/lib/components/transaction_form.dart
@@ -9,16 +9,48 @@ class TransactionForm extends StatefulWidget {
 
   TransactionForm({
     @required this.onSubmit,
-  });
+  }) {
+    print('Constructor TransactionForm');
+  }
 
   @override
-  _TransactionFormState createState() => _TransactionFormState();
+  _TransactionFormState createState() {
+    print('createState TransactionForm');
+    return _TransactionFormState();
+  }
 }
 
 class _TransactionFormState extends State<TransactionForm> {
   final _titleController = TextEditingController();
   final _valueController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
+
+  _TransactionFormState() {
+    print('Constructor _TransactionFormState');
+  }
+
+  @override 
+  void initState(){
+    super.initState();
+
+    print('initState _TransactionFormState');
+  }
+
+  @override 
+  void didUpdateWidget(TransactionForm oldWidget){
+    super.didUpdateWidget(oldWidget);
+    //widget é o componente novo, o atual, enquanto 
+    //o oldWidget é o componente velho.
+
+    print('didUpdateWidget _TransactionFormState');
+  }
+
+  @override 
+  void dispose(){
+    super.dispose();
+
+    print('dispose _TransactionFormState');
+  }
 
   void _submitForm() {
     final title = _titleController.text;
@@ -33,7 +65,7 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override 
   Widget build(BuildContext context){
-    // print('build() TransactionForm');
+    print('build() TransactionForm');
     return SingleChildScrollView(
       child: Card(
         elevation: 5,
