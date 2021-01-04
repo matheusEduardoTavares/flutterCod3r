3e027999b1543a026b46303347fb19a07549f2e1
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-01-03 21:38:11 -0300

    Aula 168 - Iniciando a APP - finalizada

---
+++ curso_flutter/meals/lib/data/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+ 
+const DUMMY_CATEGORIES = const [
+  const Category(
+    id: 'c1',
+    title: 'Italiano',
+    color: Colors.purple,
+  ),
+  const Category(
+    id: 'c2',
+    title: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  const Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  const Category(
+    id: 'c4',
+    title: 'Alemã',
+    color: Colors.amber,
+  ),
+  const Category(
+    id: 'c5',
+    title: 'Leve & Saudável',
+    color: Colors.amber,
+  ),
+  const Category(
+    id: 'c6',
+    title: 'Exótica',
+    color: Colors.green,
+  ),
+  const Category(
+    id: 'c7',
+    title: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  const Category(
+    id: 'c8',
+    title: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  const Category(
+    id: 'c9',
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  const Category(
+    id: 'c10',
+    title: 'Verão',
+    color: Colors.teal,
+  ),
+];
