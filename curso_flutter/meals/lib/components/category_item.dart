f9dcf86af25d7654e6e03334d0588f6912d426ed
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-01-03 22:16:46 -0300

    Aula 170 - Componente Item Categoria - finalizada

--- curso_flutter/meals/lib/components/category_item.dart
+++ curso_flutter/meals/lib/components/category_item.dart
@@ -9,10 +9,30 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: category.color,
-      child: Center(
-        child: Text(category.title)
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        //fazer uma borda arredondada:
+        borderRadius: BorderRadius.circular(15),
+        //Usaremos um gradiente linear para a cor 
+        //passando nossa com com uma opacidade de 50% e
+        //a cor em si. O LinearGradient possui outros 
+        //parâmetros como o begin e end para definir onde
+        //irá começar o gradiente e terminar, por exemplo
+        //do superior esquerda para inferior direita, etc.
+        //Dessa forma ele irá passar por todas as cores
+        //da lista do colors que passaros para o Linear
+        //Gradient, de cima para baixo fazendo assim o 
+        //gradiente.
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+        )
       ),
+      child: Text(category.title),
     );
   }
 }
