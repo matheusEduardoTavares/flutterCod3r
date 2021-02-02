84df6ed1e8096638d9b8ee5e8bd8d010d6a139e0
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-02-01 21:37:03 -0300

    Implementado validacao e notificacao do usuario caso nao haja nenhum item favoritado

--- curso_flutter/shop/lib/widgets/product_grid.dart
+++ curso_flutter/shop/lib/widgets/product_grid.dart
@@ -20,6 +20,29 @@ class ProductGrid extends StatelessWidget {
     final products = showFavoriteOnly ? 
       productsProvider.favoriteItems : 
       productsProvider.items;
+
+    if (products.isEmpty)
+      return Center(
+        child: Text(
+          'Não há nenhum item favoritado !',
+          style: TextStyle(
+            fontSize: 38,
+            shadows: <Shadow>[
+              Shadow(
+                blurRadius: 0.5,
+                color: Colors.red,
+                offset: Offset(1, 1)
+              ),
+              Shadow(
+                blurRadius: 0.5,
+                color: Colors.black,
+                offset: Offset(1.5, 1.5)
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
     
     return GridView.builder(
       padding: const EdgeInsets.all(10),
