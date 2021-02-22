95f552e172bd84caf31214880af63781d62ae475
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-02-21 21:47:04 -0300

    Aula 236 - Usando Snackbar - finalizada

--- curso_flutter/shop/lib/providers/cart.dart
+++ curso_flutter/shop/lib/providers/cart.dart
@@ -55,6 +55,31 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+
+    if (_items[productId].quantity == 1) {
+      _items.remove(productId);
+    }
+    else {
+      _items.update(productId, (existingItem) {
+        return CartItem(
+          id: existingItem.id,
+          // productId: existingItem.productId,
+          //Ou (tanto faz):
+          productId: productId,
+          title: existingItem.title,
+          quantity: existingItem.quantity - 1,
+          price: existingItem.price,
+        );
+      });
+    }
+
+    notifyListeners();
+  }
+
   void removeItem(String productId) {
     _items.remove(productId);
     notifyListeners();
--- curso_flutter/shop/lib/widgets/product_item.dart
+++ curso_flutter/shop/lib/widgets/product_item.dart
@@ -44,6 +44,65 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).accentColor,
             onPressed: () {
+              ///Agora ao adicionar um item no carrinho queremos
+              ///mostrar um [SnackBar] mostrando que tal item
+              ///foi adicionado ao carrinho. No [SnackBar] iremos
+              ///dar a opção para alterar a última ação realizada,
+              ///através de um botão, então se foi 
+              ///adicionado um novo item no carrinho, ao clicar
+              ///nesse botão aquele item será removido, e se 
+              ///aumentou a quantidade de um produto que já 
+              ///estava no carrinho, ao clicar no botão, a 
+              ///será retirar essa adição.
+              ///
+              ///Para acessar a tela atual, podemos usar o 
+              ///`Scaffold.of(context)`, pois será buscado na 
+              ///hierarquia até achar o primeiro Scaffold, que no
+              ///caso é o que está em [products_overview_screen.dart].
+              ///E nessa tela com o Scaffold iremos adicionar na parte
+              ///inferior da tela a Snackbar.
+              ///
+              ///Um exemplo é que podemos abrir a [Drawer] caso o 
+              ///primeiro Scaffold encontrado tiver uma [Drawer], 
+              ///ao clicar no botão:
+              // Scaffold.of(context).openDrawer();
+
+              ///Para não ter problema de ao clicar rapidamente
+              ///em adicionar vários itens ficar vindo uma snack
+              ///bar em cima da outra já que a anterior não some
+              ///até acabar seu duration, vamos programar para 
+              ///que ao clicar em adicionar um novo item, caso 
+              ///já esteja aparecendo uma SnackBar ela seja 
+              ///escondida
+              
+              Scaffold.of(context).hideCurrentSnackBar();
+
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  ///O content do SnackBar geralmente colocamos
+                  ///uma mensagem para notificar o usuário
+                  content: Text(
+                    'Produto adicionado com sucesso!',
+                    ///Nesse caso não queremos mas é possível 
+                    ///alinhar o texto como quisermos
+                    // textAlign: TextAlign.center,
+                  ),
+                  ///O tempo que o SnackBar deve ficar aparecendo
+                  ///antes de sumir
+                  duration: Duration(seconds: 2),
+                  ///A action de um SnackBar recebe uma 
+                  ///SnackBarAction que é outro Widget, e 
+                  ///geralmente aqui temos um botão para desfazer
+                  ///o que foi feito
+                  action: SnackBarAction(
+                    label: 'DESFAZER',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }
+                  )
+                )
+              );
+
               cart.addItem(product);
             },
           )
