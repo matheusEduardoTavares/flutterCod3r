6d9d829bca9c774917cc00e58b2c997a6e508339
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-03-02 22:34:29 -0300

    Aula 242 - Usando o Form do Flutter - finalizada

--- curso_flutter/shop/lib/views/product_form_screen.dart
+++ curso_flutter/shop/lib/views/product_form_screen.dart
@@ -12,6 +12,33 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
       appBar: AppBar(
         title: Text('Formulário Produto'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: ValueKey(''),
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Título',
+                ),
+                ///Atributo que serve para definir qual
+                ///será a ação do input. Ao usar o 
+                ///[TextInputAction.next], ao invés do
+                ///botão confirmar irá aparecer um botão
+                ///escrito avançar, e assim, se houverem 
+                ///mais [TextFormField] e for programado
+                ///para ir para um próximo - é o dev quem
+                ///programa isso através dos [FocusNode],
+                ///então irá para o próximo input de acordo
+                ///com o que foi definido no [FocusNode]. Se
+                ///for o último input, apenas fecha o teclado.
+                textInputAction: TextInputAction.next,
+              )
+            ],
+          )
+        ),
+      ),
     );
   }
 }
