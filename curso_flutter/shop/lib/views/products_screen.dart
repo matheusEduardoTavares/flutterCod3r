ee7edd0c8dbf8b7f0a118615c24d28e92c2adc87
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-03-27 14:31:20 -0300

    Aula 273 - Implementando Pull-To-Refresh - finalizada

--- curso_flutter/shop/lib/views/products_screen.dart
+++ curso_flutter/shop/lib/views/products_screen.dart
@@ -6,6 +6,16 @@ import 'package:shop/widgets/product_item.dart';
 import '../widgets/app_drawer.dart';
 
 class ProductsScreen extends StatelessWidget {
+
+  ///Assim:
+  // Future<void> _refreshProducts(BuildContext context) async {
+  //   await Provider.of<Products>(context, listen: false).loadProducts();
+  // }
+  ///Ou:
+  Future<void> _refreshProducts(BuildContext context) {
+    return Provider.of<Products>(context, listen: false).loadProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -25,19 +35,24 @@ class ProductsScreen extends StatelessWidget {
         ]
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.itemsCount,
-          itemBuilder: (ctx, index) => Column(
-            children: [
-              ProductItem(
-                products[index]
-              ),
-              Divider()
-            ],
-          ) 
-        )
+      body: RefreshIndicator(
+        onRefresh: () {
+          return _refreshProducts(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.itemsCount,
+            itemBuilder: (ctx, index) => Column(
+              children: [
+                ProductItem(
+                  products[index]
+                ),
+                Divider()
+              ],
+            ) 
+          )
+        ),
       )
     );
   }
