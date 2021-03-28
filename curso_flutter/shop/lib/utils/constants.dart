d779d5eec2a5afdac09d981a93ff11400eccaa5f
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-03-27 22:51:41 -0300

    Aula 282 - Criando uma Constante para URL - finalizada

---
+++ curso_flutter/shop/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  ///Essa é uma boa alternativa mas como foi usado o package 
+  ///[flutter_dotenv] para tal, esta alternativa se faz
+  ///desnecessária, até porquê expõe a URL usada.
+  static const BASE_API_URL = 'https://flutter-cod3r.firebase.com/'; 
+}
