4a07d3a78effc670f2bc7124a1603c55659d87c9
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-03-30 00:13:38 -0300

    Adicionando anotacao na classe Constants

--- curso_flutter/shop/lib/providers/auth.dart
+++ curso_flutter/shop/lib/providers/auth.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:shop/data/store.dart';
 import 'package:shop/exceptions/auth_exception.dart';
--- curso_flutter/shop/lib/utils/constants.dart
+++ curso_flutter/shop/lib/utils/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
+  ///Devido ao uso do [DotEnv] esta classe acabou não sendo usada, já que 
+  ///tudo foi feito na minha conta do firebase cujos dados estão contidos 
+  ///no meu [.env]
   static const BASE_API_URL = 'https://flutter-cod3r.firebase.com/'; 
 }
