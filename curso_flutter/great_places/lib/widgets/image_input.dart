c85d98cb3f50164708950228b34bff6c38e5878c
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-03 17:21:40 -0300

    Aula 326 - Acessando Camera - finalizada

--- curso_flutter/great_places/lib/widgets/image_input.dart
+++ curso_flutter/great_places/lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -6,6 +8,38 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    ///Primeiro importamos a lib [image_picker], aí
+    ///aqui para tirar uma foto instanciamos o 
+    ///[ImagePicker] e usamos o método [getImage] 
+    ///dele. Conseguimos definir para ele buscar 
+    ///uma foto da galeria ou tirar uma foto a partir
+    ///do atributo [source] que passamos para esse 
+    ///método [getImage], em que o [ImageSource.camera]
+    ///é para tirar foto, e o [ImageSource.gallery] é
+    ///para buscar uma imagem da galeria
+    final _picker = ImagePicker();
+    final _imageFile = await _picker.getImage(
+      source: ImageSource.camera,
+      ///Como hoje em dia temos câmeras com 108 megaPixels
+      ///que é uma imagem gigantesca, podemos restringir isso
+      ///para a necessidade da aplicação, definindo uma 
+      ///largura máxima da imagem
+      maxWidth: 600,
+    );
+
+    ///Caso o usuário feche a tela de captura de 
+    ///imagem sem tirar uma foto, o [_imageFile] 
+    ///ficará null
+    if (_imageFile != null) {
+      setState(() {
+        _storedImage = File(_imageFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -19,7 +53,16 @@ class _ImageInputState extends State<ImageInput> {
           ///Centralizando o conteúdo dentro do 
           ///[Container]
           alignment: Alignment.center,
-          child: Text('Nenhuma imagem!'),
+          child: _storedImage != null ? 
+            Image.file(
+              _storedImage,
+              ///Cobre apenas a área disponível:
+              fit: BoxFit.cover,
+              ///Devido ao [BoxFit.cover] não tem 
+              ///problema colocar para o [width] ser
+              ///o [double.infinity]
+              width: double.infinity,
+            ) : Text('Nenhuma imagem!'),
         ),
         const SizedBox(
           width: 10,
@@ -30,7 +73,7 @@ class _ImageInputState extends State<ImageInput> {
             label: Text('Tirar Foto'),
             textColor: Theme.of(context)
               .primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
