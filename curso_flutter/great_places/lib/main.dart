faa61aceed1c4f6533dea1d417f6a92cf10bf53f
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-03 15:11:06 -0300

    Aula 323 - Estrutura Inicial do Projeto - finalizada

---
+++ curso_flutter/great_places/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/place_form_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:great_places/utils/app_routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      initialRoute: AppRoutes.PLACE_LIST,
+      routes: {
+        AppRoutes.PLACE_LIST: (ctx) => PlacesListScreen(),
+        AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
---
+++ curso_flutter/great_places/lib/utils/app_routes.dart
@@ -0,0 +1,4 @@
+class AppRoutes {
+  static const String PLACE_LIST = '/place-list';
+  static const String PLACE_FORM = '/place-form';
+}
