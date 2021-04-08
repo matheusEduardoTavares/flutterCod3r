934f3e72b9d2b52eb870d304fb9b3423641ae68c
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-07 22:47:25 -0300

    Adicionado dotenv para adicao tambem da API KEY do mapbox

--- curso_flutter/great_places/lib/main.dart
+++ curso_flutter/great_places/lib/main.dart
@@ -5,8 +5,11 @@ import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
 import 'package:great_places/utils/db_util.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotEnv;
 
 Future<void> main() async {
+  await dotEnv.load();
+
   ///Com essa nova arquitetura de inicializar o [sqflite] antes de inicializar
   ///a aplicação, é necessário usar o 
   ///[WidgetsFlutterBinding.ensureInitialized()] para que seja inicializado
