bbc8271ba223b4e014eb6edfe8c0d72ced824c6f
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-08 00:47:54 -0300

    Aula 339 - Instalando Google Maps para Flutter - finalizada

--- curso_flutter/great_places/ios/Runner/AppDelegate.swift
+++ curso_flutter/great_places/ios/Runner/AppDelegate.swift
@@ -1,5 +1,9 @@
 import UIKit
 import Flutter
+//Caso fosse usado a lib google_maps_flutter, 
+//seria necessário adicionar as linhas comentadas 
+//neste arquivo aqui, sem estarem comentadas, obviamente
+// import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +11,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    //GMSServices.provideAPIKey("YOUR KEY HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
