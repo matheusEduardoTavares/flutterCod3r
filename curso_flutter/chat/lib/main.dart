351c10757e7d96dbba715d96965f365fd4a1429e
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-09 18:51:05 -0300

    Aula 350 - Tela Chat Inicial - finalizada

---
+++ curso_flutter/chat/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:chat/screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
