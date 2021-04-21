bf594c7cce85ef3ec192aa629b305e138d4ed73e
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-20 23:17:04 -0300

    Aula 361 - Integrando Autenticacao com Firebase #02 - finalizada

--- curso_flutter/chat/lib/screens/auth_screen.dart
+++ curso_flutter/chat/lib/screens/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat/models/auth_data.dart';
 import 'package:chat/widgets/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -10,23 +11,56 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  bool _isLoading = false;
+  final _auth = FirebaseAuth.instance;
   // final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   Future<void> _handleSubmit(AuthData newAuthData) async {
-    final _auth = FirebaseAuth.instance;
+    setState(() {
+      _isLoading = true;
+    });
 
     try {
+      AuthResult _authResult;
       if (newAuthData.isLogin) {
-        await _auth.signInWithEmailAndPassword(
+        _authResult = await _auth.signInWithEmailAndPassword(
           email: newAuthData.email.trim(),
           password: newAuthData.password,
         );
       }
       else {
-        await _auth.createUserWithEmailAndPassword(
+        ///Agora além de cadastrar o email e a senha,
+        ///iremos mandar também para o [Firestore]
+        ///o nome do usuário
+        _authResult = await _auth.createUserWithEmailAndPassword(
           email: newAuthData.email.trim(),
           password: newAuthData.password,
         );
+
+        final userData = {
+          'name': newAuthData.name,
+          'email': newAuthData.email,
+        };
+
+        ///Aqui caso a collection `users` não exista
+        ///no [Firestore] ela será criada. E iremos 
+        ///armazenar nela um documento a partir de
+        ///um ID. Obtemos esse ID do usuário cadastrado
+        ///que vem das requisições de criar um usuário
+        ///ou de fazer o login, pois ambos retornam 
+        ///um [AuthResult], e desse [AuthResult] basta
+        ///pegar o [.user.uid] que será tal identificador.
+        ///Então para a coleção chamaremos o método 
+        ///[document] passando como parâmetro para ele esse
+        ///[uid] do usuário, e do resultado disso chamamos
+        ///o método [setData] passando o [Map] que criamos
+        ///e que contém os dados do usuário como parâmetro
+        ///para que na criação de um usuário, já salvemos
+        ///seu [uid] na collection `users`, cujo documento
+        ///terá como chave tal [uid]
+        await Firestore.instance.collection('users')
+          .document(_authResult.user.uid)
+          .setData(userData);
       }
       ///Caso ocorra algum erro no Firebase, será 
       ///gerado a [Exception] [PlatformException]
@@ -65,6 +99,11 @@ class _AuthScreenState extends State<AuthScreen> {
     catch (err) {
       print(err);
     }
+    finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override 
@@ -72,8 +111,44 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       // key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(
-        _handleSubmit,
+      // body: AuthForm(
+      //   _handleSubmit,
+      // ),
+      ///O [Center] será para deixar nosso formulário
+      ///apenas no centro. Para que de fato a [Column]
+      ///ocupe apenas o centro, temos que definir seu
+      ///[mainAxisSize] como [MainAxisSize.min]
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ///E aqui será feito um Widget de forma que
+            ///durante a requisição fique escurecido o
+            ///formulário e com o [CircularProgressIndicator]
+            ///bem no centro
+            Stack(
+              children: [
+                AuthForm(
+                  _handleSubmit
+                ),
+                ///O [Positioned.fill] preenche toda 
+                ///a área que é ocupada da tela
+                if (_isLoading)
+                  Positioned.fill(
+                    child: Container(
+                      margin: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5)
+                      ),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
