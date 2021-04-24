ad69944ce44b91012cbd108cfe28f03da3476c24
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-04-24 01:25:18 -0300

    Aula 370 - Inserindo o Nome do Usuario #01 - finalizada

--- curso_flutter/chat/lib/widgets/message_bubble.dart
+++ curso_flutter/chat/lib/widgets/message_bubble.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   const MessageBubble(
     this.message,
+    this.userId,
     this.belongsToMe,
     {
       this.key,
@@ -15,11 +17,15 @@ class MessageBubble extends StatelessWidget {
   ///valores de forma correta e evitamos problemas de 
   ///excluir elementos, de estado.
   final Key key;
+  final String userId;
   final String message;
   final bool belongsToMe;
 
   @override
   Widget build(BuildContext context) {
+    Color _defaultTextColor = belongsToMe ? Colors.black : 
+      Theme.of(context).accentTextTheme.headline1.color;
+
     return Row(
       mainAxisAlignment: belongsToMe ? MainAxisAlignment.end :
         MainAxisAlignment.start,
@@ -50,18 +56,67 @@ class MessageBubble extends StatelessWidget {
             vertical: 10.0,
             horizontal: 16.0,
           ),
-          child: Text(
-            message,
-            style: TextStyle(
-              ///A partir da versão 1.17 acessamos 
-              ///a cor do texto baseada no Accent
-              ///pegando seu 
-              ///[.accentTextTheme.headline1.color],
-              ///é uma cor que contrasta bem com o 
-              ///[AccentColor]
-              color: belongsToMe ? Colors.black : 
-                Theme.of(context).accentTextTheme.headline1.color,
-            ),
+          child: Column(
+            crossAxisAlignment: belongsToMe ? 
+              CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              ///Essa primeira estratégia que usamos para 
+              ///mostrar o nome do usuário responsável 
+              ///pela mensagem não é das melhores, 
+              ///pois embora seja feito cache das informações,
+              ///para cada mensagem teremos um [FutureBuilder]
+              ///fazendo uma chamada no [Firebase], e não é 
+              ///uma estratégia muito interessante, pois apesar
+              ///que será feito esses carregamentos apenas para
+              ///as mensagens que estão aparecendo na tela,
+              ///toda hora ficaríamos requisitando coisas.
+              ///Por isso é interessante passar por parâmetro 
+              ///o nome do usuário. Por isso uma estratégia 
+              ///legal para se fazer é colocar o nome do 
+              ///usuário no momento que formos criar uma 
+              ///nova mensagem. Iremos replicar os dados 
+              ///pois salvaremos em vários locais o nome
+              ///do usuário, mas quando trabalhamos com 
+              ///banco não relacionais, acabamos tendo
+              ///essas replicações de dados. Ganhamos 
+              ///praticidade e escalabilidade muito 
+              ///maior que em um banco relacional,
+              ///mas tem os lados negativos como
+              ///esse de replicação dos dados
+              FutureBuilder(
+                ///Pegamos os dados de um certo usuário
+                ///assim:
+                future: Firestore.instance.collection('users')
+                  .document(userId).get(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Text('Loading...');
+                  }
+
+                  return Text(
+                    snapshot.data['name'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: _defaultTextColor,
+                    ),
+                  );
+                }
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  ///A partir da versão 1.17 acessamos 
+                  ///a cor do texto baseada no Accent
+                  ///pegando seu 
+                  ///[.accentTextTheme.headline1.color],
+                  ///é uma cor que contrasta bem com o 
+                  ///[AccentColor]
+                  color: _defaultTextColor,
+                ),
+                textAlign: belongsToMe ? 
+                  TextAlign.end : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
