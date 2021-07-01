a7a828bb651ba81de8dedb0565804419504daf4c
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-06-30 22:06:23 -0300

    Aula 425 - Componente Tabuleiro - finalizada

---
+++ curso_flutter/campo_minado/lib/components/board_widget.dart
@@ -0,0 +1,34 @@
+import 'package:campo_minado/components/field_widget.dart';
+import 'package:campo_minado/models/trabuleiro.dart';
+import 'package:flutter/material.dart';
+
+class BoardWidget extends StatelessWidget {
+  BoardWidget({ 
+    @required this.onChangeMarcation,
+    @required this.board,
+    @required this.onOpen,
+    Key key,
+  }) : super(key: key);
+
+  final Tabuleiro board;
+  final ChangeMarcationHook onChangeMarcation;
+  final OpenHook onOpen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView.count(
+        ///Quantidade de colunas que teremos 
+        ///dentro da grid
+        crossAxisCount: board.colunas,
+        children: board.campos.map(
+          (field) => FieldWidget(
+            field: field, 
+            onOpen: onOpen, 
+            onChangeMarcation: onChangeMarcation,
+          ),
+        ).toList(),
+      ),
+    );
+  }
+}
--- curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
+++ curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
@@ -1,7 +1,7 @@
-import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/components/board_widget.dart';
+import 'package:campo_minado/models/trabuleiro.dart';
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
-import '../components/field_widget.dart';
 import '../models/campo.dart';
 
 class CampoMinadoApp extends StatelessWidget {
@@ -21,29 +21,29 @@ class CampoMinadoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final field = Campo(
-      linha: 0,
-      coluna: 0,
-    );
+    // final field = Campo(
+    //   linha: 0,
+    //   coluna: 0,
+    // );
 
-    final otherField1 = Campo(
-      linha: 1,
-      coluna: 0,
-    );
-    final otherField2 = Campo(
-      linha: 1,
-      coluna: 1,
-    );
-    otherField1.minar();
-    otherField2.minar();
-    field.adicionarVizinho(otherField1);
-    field.adicionarVizinho(otherField2);
+    // final otherField1 = Campo(
+    //   linha: 1,
+    //   coluna: 0,
+    // );
+    // final otherField2 = Campo(
+    //   linha: 1,
+    //   coluna: 1,
+    // );
+    // otherField1.minar();
+    // otherField2.minar();
+    // field.adicionarVizinho(otherField1);
+    // field.adicionarVizinho(otherField2);
 
-    try {
-      // field.minar();
-      // field.abrir();
-      field.alternarMarcacao();
-    } on ExplosaoException {}
+    // try {
+    //   // field.minar();
+    //   // field.abrir();
+    //   field.alternarMarcacao();
+    // } on ExplosaoException {}
 
     return MaterialApp(
       home: Scaffold(
@@ -52,8 +52,12 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: FieldWidget(
-            field: field,
+          child: BoardWidget(
+            board: Tabuleiro(
+              linhas: 15,
+              colunas: 15,
+              qtdeBombas: 10,
+            ),
             onOpen: _open,
             onChangeMarcation: _changeMarcation,
           ),
