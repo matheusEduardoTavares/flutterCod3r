118eb58385d3aa37c7a6acd08ddcf2f256bc0c04
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-06-30 22:15:46 -0300

    Aula 426 - Componente Stateful - finalizada

--- curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
+++ curso_flutter/campo_minado/lib/screens/campo_minado_app.dart
@@ -4,8 +4,20 @@ import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
 import '../models/campo.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
   const CampoMinadoApp({ Key key }) : super(key: key);
+
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  var _tabuleiro = Tabuleiro(
+    linhas: 12, 
+    colunas: 12, 
+    qtdeBombas: 3,
+  );
 
   void _reiniciar() {
     print('reiniciar');
@@ -48,16 +60,12 @@ class CampoMinadoApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: null,
+          venceu: _venceu,
           onReiniciar: _reiniciar,
         ),
         body: Container(
           child: BoardWidget(
-            board: Tabuleiro(
-              linhas: 15,
-              colunas: 15,
-              qtdeBombas: 10,
-            ),
+            board: _tabuleiro,
             onOpen: _open,
             onChangeMarcation: _changeMarcation,
           ),
